ee1d1f0ba8c57a431b8d28bedcce86895ef71f80
gitjona <89576059+gitjona@users.noreply.github.com>
2021-11-16 13:58:21 +0200

    Add files via upload

--- src/components/Kyselylist.js
+++ src/components/Kyselylist.js
@@ -2,17 +2,40 @@ import React from 'react';
 
 function Kyselylist()
 {
+    const [id, setId] = React.useState('');
+
     const [title, setTitle] = React.useState('');
 
-    React.useEffect(() => {
-        fetch('https://saerarjojo.herokuapp.com/kysely/1')
-        .then(response => response.json())
-        .then(data => setTitle(data.title))
-        .catch(err => console.log(err))
+    React.useEffect(() => {    
+
+        fetchQuestion();
+
     }, [])
+
+    function fetchQuestion()
+    {
+        fetch('https://saerarjojo.herokuapp.com/kysely/' + id)
+        .then(response => response.json())
+        .then(data => {
+             setTitle(data.title)
+        })
+        .catch(err => console.log(err))
+    }
+
+    function inputChanged (event)
+    {
+        event.preventDefault();
+    }
 
     return(
         <div>
+            <h3>Kysely {id}</h3>
+            
+            <form onSubmit={inputChanged}>
+                <input value={id} onChange={event => setId(event.target.value)}/>
+                <button onClick={fetchQuestion}>Set id</button>
+            </form>
+
             <p>Title: {title}</p>
         </div>
     );

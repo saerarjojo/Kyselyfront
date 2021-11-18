ab286b088b8e6e5c90e9c619250071f16d0af8aa
gitjona <89576059+gitjona@users.noreply.github.com>
2021-11-18 11:39:41 +0200

    Add files via upload

--- src/components/Kyselylist.js
+++ src/components/Kyselylist.js
@@ -5,51 +5,45 @@ function Kyselylist()
     const [id, setId] = React.useState('');
 
     const [title, setTitle] = React.useState('');
-    const [questions, setQuestions] = React.useState([]);
-    const [qId, setQId] = React.useState('0');
+
+    var table = [];
+    var text = "";
 
     React.useEffect(() => {    
 
         fetchQuestion();
 
-    }, [])
+    }, )
 
     function fetchQuestion()
     {
         fetch('https://saerarjojo.herokuapp.com/kysely/' + id)
         .then(response => response.json())
         .then(data => {
-
+            
             for(let i=0; i < data.kysymykset.length; i++)
             {
-                setQId(i);
-
                 setTitle(data.title);
-                setQuestions(data.kysymykset[qId].kysymys);
+                
+                table += data.kysymykset[String(i)].kysymys;
+
+                document.getElementById("demo").innerHTML = table;
             }
         })
         .catch(err => console.log(err))
-
-    }
-
-    function inputChanged (event)
-    {
-        event.preventDefault();
     }
 
     return(
         <div>
             <h3>Kysely {id}</h3>
             
-            <form onSubmit={inputChanged}>
+            <form>
                 <input value={id} onChange={event => setId(event.target.value)}/>
-                <button onClick={fetchQuestion}>Set id</button>
             </form>
 
             <p>Title: {title}</p>
             <p>Kysymykset:</p>
-            <p>{questions}</p>
-            
+            <p id="demo"></p>          
         </div>
     );
 }
